ec5a4dadde864e41314da81f4ab7e3c275f77cf8
cogcamp <codeofgenius.camp@gmail.com>
2021-12-28 06:46:51 +0000

    manato

--- manato/start.js
+++ manato/start.js
@@ -4,7 +4,7 @@ var startScene = new Phaser.Scene("startScene");
 startScene.create = function () {
     // スタートボタン画像を表示する
     this.startButtonImage = this.add.image(400, 300, 'startButton');
-
+    alert("Hello!!")
     // キーをクリックするとゲームスタート
     this.input.keyboard.on('keydown', function(event) {
         this.scene.start('mainScene');
